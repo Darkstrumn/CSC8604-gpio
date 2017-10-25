55a731ea9e64a4f366481efc5455fa2f829d9d13
Dan <d.howard2@ncl.ac.uk>
2017-10-25 11:39:26 +0000

    Digital Input

--- input.js
+++ input.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+var wpi = require('wiring-pi');
+wpi.setup('phys');
+var pin = 7;
+wpi.pinMode(pin, wpi.INPUT);
+wpi.pullUpDnControl(pin, wpi.PUD_UP);
+
+function read() {
+	var value = wpi.digitalRead(pin);
+	console.log(value);
+	}
+
+wpi.wiringPiISR(pin, wpi.INT_EDGE_BOTH, read);
